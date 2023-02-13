4b15e0a2a2a2791eee6176c593ec61b9a7713047
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-02-13 22:39:31 +0300

    create Nav and Banner components

---
+++ src/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Banner.css";
+
+function Banner() {
+  return (
+      <header className='banner' style={{
+          backgroundSize:"cover",
+          backgroundImage: `url("https://bilisimgundem.com/wp-content/uploads/2020/02/netflix-banner-1.jpg")`,
+          backgroundPosition:"center center",
+          
+      }}>
+          
+    </header>
+  )
+}
+
+export default Banner
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5gUl3FywMENEroyB99yGgvjEdYCs7wTA",
+    authDomain: "netflix-clone-23723.firebaseapp.com",
+    projectId: "netflix-clone-23723",
+    storageBucket: "netflix-clone-23723.appspot.com",
+    messagingSenderId: "899892361031",
+    appId: "1:899892361031:web:bfb65b243a850eeadafe62"
+  };
