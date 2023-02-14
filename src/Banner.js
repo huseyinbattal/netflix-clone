1a8c5a72e2d16b0b00f4748f50be34dc56325da6
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-02-14 23:32:12 +0300

    create banner.css file

--- src/Banner.js
+++ src/Banner.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from "react";
 import "./Banner.css";
 
 function Banner() {
   return (
-      <header className='banner' style={{
-          backgroundSize:"cover",
-          backgroundImage: `url("https://bilisimgundem.com/wp-content/uploads/2020/02/netflix-banner-1.jpg")`,
-          backgroundPosition:"center center",
-          
-      }}>
-          
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://bilisimgundem.com/wp-content/uploads/2020/02/netflix-banner-1.jpg")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">Movie Name</h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner__description">This is test description </h1>
+      </div>
+      <div className="banner--fadeBottom"/>
     </header>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
