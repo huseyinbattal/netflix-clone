3fe90386ba7fcd822cd7aa0ee7435e72aa039ea3
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-02-18 02:42:20 +0300

    create the LoginScreen component

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./LoginScreen.css";
+import netflix_logo from "../picture/netflix_logo.png"
+import SignupScreen from "./SignupScreen";
 
 function LoginScreen() {
+  const [signIn, setSignIn] = useState(false);
   return (
-      <div className='loginScreen'>
-          
+    <div className="loginScreen">
+      <div className="loginScreen__background">
+        <img
+          className="loginScreen__logo"
+          src={netflix_logo}
+          alt="logo"
+        />
+        <button
+        onClick={()=>setSignIn(true)}
+          className="loginScreen__button">Sign In</button>
+        <div className="loginScreen__gradient"></div>
+        <div className="loginScreen__body">
+          {signIn ? (
+              <SignupScreen/>
+          ): (
+            <>
+            <h1>Unlimited movies, TV shows, and more.</h1>
+            <h2>Watch anywhere. Cancel anytime.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h3>
+            <div className="loginScreen__input">
+              <form>
+                <input
+                  type="email"
+                placeholder="Email Address"
+                />
+                <button
+                  onClick={()=>setSignIn(true)}
+                  className="loginScreen__getStarted">GET STARTED</button>
+              </form>
+            </div>
+          </>
+          )}
+         
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default LoginScreen
+export default LoginScreen;
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./SignupScreen.css"
+
+function SignupScreen() {
+  return (
+      <div className='signupScreen'>
+          
+    </div>
+  )
+}
+
+export default SignupScreen
