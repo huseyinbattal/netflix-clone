f444a929a6416941204b7a9d8b101a180fda93ed
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-02-19 02:22:57 +0300

    implement redux

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./features/user/userSlice";
 
 function App() {
   const user = null;
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
           //Logged in
-        console.log(userAuth);
+        dispatch(login({
+          uid: userAuth.uid,
+          email:userAuth.email,
+        }))
       } else {
           //Logged out
+        dispatch(logout)
         }
     })
     
