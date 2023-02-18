1da92b97889dad52f590a4db433ff442cd63b44d
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-02-19 01:11:35 +0300

    create the SignupScreen component and set up firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+          //Logged in
+        console.log(userAuth);
+      } else {
+          //Logged out
+        }
+    })
+    
+    return unsubscribe;
+
+  }, [])
+  
   return (
     <div className="app">
       <Router>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyB5gUl3FywMENEroyB99yGgvjEdYCs7wTA",
     authDomain: "netflix-clone-23723.firebaseapp.com",
@@ -5,4 +9,11 @@ const firebaseConfig = {
     storageBucket: "netflix-clone-23723.appspot.com",
     messagingSenderId: "899892361031",
     appId: "1:899892361031:web:bfb65b243a850eeadafe62"
-  };
+};
+  
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
